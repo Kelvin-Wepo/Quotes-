2ae99d9fccb2decc024d7fa724fdce3cd197227a
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-04-11 19:14:17 +0300

    fix;this commit fixed upvotes and downvotes section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
