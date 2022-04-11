9a9d27c03a73628ef5b4d43738ffa2751a3a5e57
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-04-11 18:11:13 +0300

    feat;this commit added upvotes and downvotes sections

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -3,12 +3,12 @@ export class Quotes {
     author!:string;
     quote!:string;
     by!:string;
-    date!:number;
+    date!:Date;
     upvote!:number;
     downvote!:number;
     show!:boolean
 
-    constructor(id:number,author:string,quote:string,by:string,date:number,upvote:number,downvote:number){
+    constructor(id:number,author:string,quote:string,by:string,date:Date,upvote:number,downvote:number){
      this.show=false; 
      this.id=id;
      this.author=author;
