5c02c3a0221fa7185d21fd4e9d8b12fe00dfb10d
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-04-11 14:28:05 +0300

    feat;this commit added class properties

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,21 @@
 export class Quotes {
+    id!:number;
+    author!:string;
+    quote!:string;
+    by!:string;
+    date!:number;
+    upvote!:number;
+    downvote!:number;
+    show!:boolean
+
+    constructor(id:number,author:string,quote:string,by:string,date:number,upvote:number,downvote:number){
+     this.show=false; 
+     this.id=id;
+     this.author=author;
+     this.quote=quote;
+     this.by=by;
+     this.date=date;
+     this.upvote=upvote;
+     this.downvote;
+    }
 }
