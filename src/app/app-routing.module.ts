8ee7c52c99caeb4b2a2e67c14ea0a09e1ad7ff01
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-04-11 13:09:55 +0300

    fx.fix quote.html component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
 const routes: Routes = [];
 
