83fef788a90afbde89d5e9524e16916991cc8367
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-04-21 19:29:27 +0300

    feat

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Output,EventEmitter} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-form',
@@ -6,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  newQuote =new Quotes(0,"","","",new Date(),0,0);
+  @Output() addQuote =new EventEmitter<Quotes>();
 
+  // emmit data to parent component
+  submitQuote() { 
+    this.addQuote.emit(this.newQuote);
+  }
+
+  
   constructor() { }
 
   ngOnInit(): void {
